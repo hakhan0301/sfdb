788ffbe98f26003ae13fd5b81552fba8e69d3ac0
Hassan <hakhan0301@gmail.com>
2022-07-12 01:31:50 -0500

    login route

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,23 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
 
 import HomeScreen from './screens/Home';
+import LoginScreen from './screens/Login';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NativeBaseProvider>
+      <StatusBar barStyle="light-content" />
       <NavigationContainer>
-
         <Stack.Navigator
-          screenOptions={{ header: () => <></> }}
+          screenOptions={{
+            header: () => <></>,
+            animation: 'none'
+          }}
         >
           <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
         </Stack.Navigator>
-        <StatusBar style="auto" />
+
       </NavigationContainer>
     </NativeBaseProvider>
   );
---
+++ apps/mobile/screens/Login.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "native-base";
+import { ScreenProps } from "./types";
+
+export default function Login({ navigation }: ScreenProps) {
+
+  return (
+    <Box>
+      <Text>sussy</Text>
+    </Box>
+  )
+
+}
