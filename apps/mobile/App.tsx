9b5ec2adb90a06e4f451c6fc8633416665cb6482
Hassan <hakhan0301@gmail.com>
2022-07-11 21:29:49 -0500

    tailwind setup

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Sussy baka app</Text>
+    <View style={tw`w-full h-full flex justify-center items-center`}>
+      <Text style={tw``}>Sussy baka app2</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
