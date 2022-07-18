636dacba630a35458fdd8b8c4f37ab05b7bb2d52
Hassan <hakhan0301@gmail.com>
2022-07-18 17:22:32 -0500

    fix: useSession errors on no session

--- apps/mobile/libs/hooks/auth.ts
+++ apps/mobile/libs/hooks/auth.ts
@@ -3,5 +3,8 @@ import AuthContext from '../contexts/AuthContext';
 
 export function useSession() {
   const session = useContext(AuthContext);
+
+  if (!session) throw new Error('No session available.');
+
   return session;
 }
