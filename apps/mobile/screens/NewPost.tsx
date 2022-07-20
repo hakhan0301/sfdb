55a3cf347932e7c66676466dc5d06647193faad6
Hassan <hakhan0301@gmail.com>
2022-07-19 23:13:37 -0500

    feature: started new post page

--- apps/mobile/screens/NewPost.tsx
+++ apps/mobile/screens/NewPost.tsx
@@ -1,50 +1,68 @@
-import { Box, Row, ScrollView, Text } from 'native-base';
+import { Box, Button, Center, Column, Row, ScrollView, Text, View } from 'native-base';
 import React, { useState } from 'react';
 import Swiper from 'react-native-swiper';
 import tw from 'twrnc';
+import { AntDesign } from '@expo/vector-icons';
 
 import type { Post as PostType } from 'src/libs/types/posts';
 import type { ScreenProps } from 'src/libs/types/screen';
-
-import Post from 'src/libs/ui/Post';
-
-
+import { LinearGradient } from 'expo-linear-gradient';
+import { BlurView } from 'expo-blur';
 
 export default function Home({ navigation }: ScreenProps) {
   const [index, setIndex] = useState(0);
 
-  const [posts, setPosts] = useState<PostType[]>([
-    {
-      id: 0, title: 'is this SUSSY?', createdAt: new Date(), likes: 1, text: 'https://steamuserimages-a.akamaihd.net/ugc/1772707788143102214/2EB84E86640D917614EB57D517AFB3FB2F024A4C/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
-      user: { name: 'OME', streaks: 12, strikes: 1, pfp: 'https://pbs.twimg.com/profile_images/1408387150119243778/j14N_zq__400x400.jpg', },
-      comments: [
-        { id: 0, createdAt: new Date(), text: 'this is ome susy', user: { name: 'memon', pfp: 'https://avatars.githubusercontent.com/u/21112116?s=120&v=4' } },
-        { id: 2, createdAt: new Date(), text: 'ia m shan', user: { name: 'memon', pfp: 'https://avatars.githubusercontent.com/u/21112116?s=120&v=4' } },
-        { id: 1, createdAt: new Date(), text: 'jasdfkljfsdjk', user: { name: 'memon', pfp: 'https://avatars.githubusercontent.com/u/21112116?s=120&v=4' } },
-      ]
-    },
-    {
-      id: 1, title: 'this is pretty SuSy', createdAt: new Date(), likes: 12, text: 'https://pbs.twimg.com/media/EzsXbgtXMAEmqIr.jpg',
-      user: { name: 'OMEgaga', streaks: 1, strikes: 2, pfp: 'https://pbs.twimg.com/profile_images/1408387150119243778/j14N_zq__400x400.jpg', },
-      comments: [
-        { id: 0, createdAt: new Date(), text: 'this fdsaf', user: { name: 'memon2', pfp: 'https://avatars.githubusercontent.com/u/21112116?s=120&v=4' } },
-        { id: 2, createdAt: new Date(), text: 'ia m f shan', user: { name: 'memon2', pfp: 'https://avatars.githubusercontent.com/u/21112116?s=120&v=4' } },
-        { id: 1, createdAt: new Date(), text: 'wahooo', user: { name: 'memon2', pfp: 'https://avatars.githubusercontent.com/u/21112116?s=120&v=4' } },
-      ]
-    },
-  ]);
-
   return (
-    <>
-      <Swiper
-        showsPagination={false} loadMinimal={true}
-        horizontal={false} loop={false} index={index} onIndexChanged={setIndex}>
-        {posts.map((post, i) => (
-          <ScrollView key={post.id} nestedScrollEnabled={true} scrollEnabled={Math.abs(index - 1) <= 1}>
-            <Post {...post} />
-          </ScrollView>
-        ))}
-      </Swiper>
-    </>
+    <LinearGradient
+      colors={['#ff7043', '#e040fb']}
+      style={tw``}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+    >
+      <Column style={tw`w-full h-full`}>
+        <BlurView style={tw`m-6 rounded-3xl border-4 border-white`} intensity={50}>
+          <Row space="5"
+            style={tw`p-5 items-center`}>
+            <AntDesign name="clockcircleo" size={96} color="white" />
+
+            <Column>
+              <Text style={tw`text-4xl text-white -mb-2`}>3 hours</Text>
+              <Text style={tw`text-white text-base`}>remaining</Text>
+            </Column>
+          </Row>
+        </BlurView>
+
+        <View style={tw`bg-stone-800 flex-grow mt-5 p-4 pt-9`}>
+          <LinearGradient
+            colors={['#1e88e5', '#42a5f5']}
+            style={tw`absolute -top-5 self-center rounded-xl`}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+          >
+
+            <Row style={tw`rounded-xl`}>
+              <Button onPress={() => setIndex(0)}
+                style={tw`${index == 0 ? 'bg-yellow-400' : 'bg-white/0'} w-24 rounded-l-lg rounded-r-none`}>
+                <Text style={tw`${index == 0 ? 'text-black' : 'text-white'} font-bold`}>Text</Text>
+              </Button>
+              <Button onPress={() => setIndex(1)}
+                style={tw`${index == 1 ? 'bg-yellow-400' : 'bg-white/0'} w-24 rounded-none`}>
+                <Text style={tw`${index == 1 ? 'text-black' : 'text-white'} font-bold`}>Text</Text>
+              </Button>
+              <Button onPress={() => setIndex(2)}
+                style={tw`${index == 2 ? 'bg-yellow-400' : 'bg-white/0'} w-24 rounded-r-lg rounded-l-none`}>
+                <Text style={tw`${index == 2 ? 'text-black' : 'text-white'} font-bold`}>Text</Text>
+              </Button>
+            </Row>
+          </LinearGradient>
+          <Text style={tw`text-white`}>fasdfsd</Text>
+
+        </View>
+        <View style={tw`h-12`}>
+          <Text>fsd</Text>
+        </View>
+
+      </Column>
+    </LinearGradient>
   );
 } 
