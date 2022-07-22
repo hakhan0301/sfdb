38c8d693a0540b0f0bc49096bf282bda7306a3f9
Hassan <hakhan0301@gmail.com>
2022-07-21 21:34:33 -0500

    refactor: pulled out buttonof3

---
+++ apps/mobile/libs/ui/newPost/Buttons.tsx
@@ -0,0 +1,31 @@
+import { Button, Text } from 'native-base';
+import React from 'react';
+import tw from 'twrnc';
+
+import { ButtonProps } from "../types";
+
+export interface ButtonOf3Props extends ButtonProps {
+  index: 0 | 1 | 2;
+  isActive: boolean;
+}
+
+export function ButtonOf3({ text, onPress, index, isActive }: ButtonOf3Props) {
+  let roundedStyle = {
+    0: 'rounded-l-lg rounded-r-none',
+    1: 'rounded-none',
+    2: 'rounded-r-lg rounded-l-none'
+  }
+
+  const style = tw`${isActive ? 'bg-yellow-300' : 'bg-white/0'} ${roundedStyle[index]}`
+
+  return (
+    <Button onPress={onPress}
+      w='24'
+      _pressed={{
+        style: { ...style, ...tw`${isActive ? 'bg-yellow-400' : 'bg-black/10'}` },
+      }}
+      style={style}>
+      <Text style={tw`${isActive ? 'text-black' : 'text-white'} font-bold`}>{text}</Text>
+    </Button>
+  )
+}
--- apps/mobile/screens/NewPost.tsx
+++ apps/mobile/screens/NewPost.tsx
@@ -1,39 +1,14 @@
-import { Box, Button, Center, Column, Row, ScrollView, Text, View } from 'native-base';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
+import { Button, Column, Row, Text, View } from 'native-base';
 import React, { useState } from 'react';
 import tw from 'twrnc';
-import { AntDesign } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
 
-import type { ScreenProps } from 'src/libs/types/screen';
-import { LinearGradient } from 'expo-linear-gradient';
-import { BlurView } from 'expo-blur';
 import { FontAwesome5 } from '@expo/vector-icons';
-import { ButtonProps } from 'src/libs/ui/types';
+import { BlurView } from 'expo-blur';
+import { LinearGradient } from 'expo-linear-gradient';
+import type { ScreenProps } from 'src/libs/types/screen';
+import { ButtonOf3 } from 'src/libs/ui/newPost/Buttons';
 
-interface ButtonOf3Props extends ButtonProps {
-  index: 0 | 1 | 2;
-  isActive: boolean;
-}
-function ButtonOf3({ text, onPress, index, isActive }: ButtonOf3Props) {
-  let roundedStyle = {
-    0: 'rounded-l-lg rounded-r-none',
-    1: 'rounded-none',
-    2: 'rounded-r-lg rounded-l-none'
-  }
-
-  const style = tw`${isActive ? 'bg-yellow-300' : 'bg-white/0'} ${roundedStyle[index]}`
-
-  return (
-    <Button onPress={onPress}
-      w='24'
-      _pressed={{
-        style: { ...style, ...tw`${isActive ? 'bg-yellow-400' : 'bg-black/10'}` },
-      }}
-      style={style}>
-      <Text style={tw`${isActive ? 'text-black' : 'text-white'} font-bold`}>{text}</Text>
-    </Button>
-  )
-}
 
 type ActiveSections = 'Text' | 'Link' | 'Image';
 
