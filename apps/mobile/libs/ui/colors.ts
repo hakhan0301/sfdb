0ba30d7afd157ecd561c1251e8c0d29053689e7d
Hassan <hakhan0301@gmail.com>
2022-07-21 22:16:55 -0500

    new colors

--- apps/mobile/libs/ui/colors.ts
+++ apps/mobile/libs/ui/colors.ts
@@ -10,3 +10,43 @@ export const yellow600 = '#fdd835'
 export const yellow700 = '#fbc02d'
 export const yellow800 = '#f9a825'
 export const yellow900 = '#f57f17'
+
+export const purple = '#9c27b0';
+export const purple50 = '#f3e5f5';
+export const purple100 = '#e1bee7';
+export const purple200 = '#ce93d8';
+export const purple300 = '#ba68c8';
+export const purple400 = '#ab47bc';
+export const purple500 = '#9c27b0';
+export const purple600 = '#8e24aa';
+export const purple700 = '#7b1fa2';
+export const purple800 = '#6a1b9a';
+export const purple900 = '#4a148c';
+export const purple100accent = '#ea80fc';
+export const purple200accent = '#e040fb';
+
+export const deepOrange = '#ff5722';
+export const deepOrange50 = '#fbe9e7';
+export const deepOrange100 = '#ffccbc';
+export const deepOrange200 = '#ffab91';
+export const deepOrange300 = '#ff8a65';
+export const deepOrange400 = '#ff7043';
+export const deepOrange500 = '#ff5722';
+export const deepOrange600 = '#f4511e';
+export const deepOrange700 = '#e64a19';
+export const deepOrange800 = '#d84315';
+export const deepOrange900 = '#bf360c';
+
+export const blue = '#2196f3';
+export const blue50 = '#e3f2fd';
+export const blue100 = '#bbdefb';
+export const blue200 = '#90caf9';
+export const blue300 = '#64b5f6';
+export const blue400 = '#42a5f5';
+export const blue500 = '#2196f3';
+export const blue600 = '#1e88e5';
+export const blue700 = '#1976d2';
+export const blue800 = '#1565c0';
+export const blue900 = '#0d47a1';
+
+
