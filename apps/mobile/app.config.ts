a99b487a36aa8fcd4354f070043306e91426fba0
Hassan <hakhan0301@gmail.com>
2022-07-25 00:09:18 -0500

    refactor: app.config => ts

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -1,6 +1,9 @@
-export default {
+import { ExpoConfig, ConfigContext } from '@expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
   name: 'sFoolar DB',
   version: '1.0.0',
+  slug: 'sFDB',
   extra: {
     SUPABASE_URL: 'https://xrogczgyhvzygyoszmxr.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhyb2djemd5aHZ6eWd5b3N6bXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTc2MDA2NzUsImV4cCI6MTk3MzE3NjY3NX0.osJkGfF5AFgiKwxjX7e_B0Ye09xc2dhaFXyCVLBBs7g',
@@ -8,4 +11,4 @@ export default {
   android: {
     package: "hakhan0301.sfdb"
   }
-};
+});
