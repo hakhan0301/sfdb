0ba6a4838a62073081f144a9a840ec0fe2371fd2
Hassan <hakhan0301@gmail.com>
2022-09-04 20:37:25 -0500

    feat: added function env vars

--- apps/functions/bin/functions.ts
+++ apps/functions/bin/functions.ts
@@ -3,6 +3,10 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { FunctionsStack } from '../lib/functions-stack';
 
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
+
 const app = new cdk.App();
 new FunctionsStack(app, 'FunctionsStack', {
   env: {
--- apps/functions/lib/functions-stack.ts
+++ apps/functions/lib/functions-stack.ts
@@ -7,18 +7,35 @@ import * as path from 'path';
 
 const FUNCTIONS_SOURCE_PATH = path.join(__dirname, '..', 'src');
 
+type EnvironmentVariables = { [key: string]: string };
+
+function getEnvVariables(varNames: string[]) {
+  let vars: EnvironmentVariables = {};
+
+  for (const varName of varNames) {
+    const envVar = process.env[varName];
+    if (!envVar) throw new Error(`Missing env-var: "${varName}"`);
+    vars[varName] = envVar;
+  }
+
+  return vars;
+}
+
 export class FunctionsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    this.createFunction('WarnUsers');
+    this.createFunction('WarnUsers',
+      getEnvVariables(['FUNCTION_SECRET', 'SUPABASE_URL', 'SUPABASE_ADMIN_KEY'])
+    );
   }
 
-  createFunction(functionName: string) {
+  createFunction(functionName: string, env?: EnvironmentVariables) {
     const fn = new lambda.NodejsFunction(this, functionName, {
       functionName: `sfdb-${functionName}`,
       entry: path.join(FUNCTIONS_SOURCE_PATH, functionName, 'index.ts'),
       handler: 'handler',
-      bundling: { minify: false }
+      bundling: { minify: false },
+      environment: env
     });
 
     const functionUrl = new CfnResource(this, `${functionName}-URL`, {
