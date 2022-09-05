8d6bcaf7a5ecfc9f52615047af829898e2ac71c4
Hassan <hakhan0301@gmail.com>
2022-09-04 21:13:25 -0500

    feature: fixes warning timezone

--- apps/functions/src/WarnUsers/index.ts
+++ apps/functions/src/WarnUsers/index.ts
@@ -1,8 +1,7 @@
 import { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
-import * as moment from 'moment/moment';
-import 'moment-timezone';
+import * as moment from 'moment-timezone';
 
-import { ExpoPushMessage } from 'expo-server-sdk/build/ExpoClient';
+import { ExpoPushMessage, ExpoPushTicket } from 'expo-server-sdk/build/ExpoClient';
 import { Expo } from 'expo-server-sdk';
 
 import { tables } from '@sfdb/supabase';
@@ -11,6 +10,8 @@ if (!process.env.FUNCTION_SECRET)
   throw new Error('Missing env-var: "FUNCTION_SECRET"');
 
 const expo = new Expo();
+
+process.env.TZ = 'America/Mexico_City';
 
 export const handler = async (
   event: APIGatewayEvent
@@ -24,10 +25,11 @@ export const handler = async (
     .lt('strikes', 3)
     .not('notification_token', 'eq', null);
 
-  if (error) return {
-    body: JSON.stringify(error),
-    statusCode: 500
-  }
+  if (error)
+    return {
+      body: JSON.stringify(error),
+      statusCode: 500
+    }
 
   const timeTillStrike = timeUntilTomorrow();
 
@@ -39,19 +41,21 @@ export const handler = async (
 
   const chunks = expo.chunkPushNotifications(pushMessages);
 
+  let results: ExpoPushTicket[] = [];
   for (const chunk of chunks) {
-    await expo.sendPushNotificationsAsync(chunk);
+    const res = await expo.sendPushNotificationsAsync(chunk);
+    results.push(...res);
   }
 
   return {
-    body: JSON.stringify("Success"),
+    body: JSON.stringify(results),
     statusCode: 200
   }
 }
 
 function unauthorized(key: string | undefined): APIGatewayProxyResult {
   return {
-    body: `Invalid "key" query string. Received: ${key}`,
+    body: `Invalid "key" in querystring. Received: ${key}`,
     statusCode: 400
   }
 }
@@ -68,7 +72,6 @@ function todayMorning() {
 
 function timeUntilTomorrow() {
   const now = getNow();
-  now.setSeconds(0, 0);
   const tomorrow = getNow();
   tomorrow.setDate(now.getDate() + 1);
   tomorrow.setHours(0, 0, 0);
