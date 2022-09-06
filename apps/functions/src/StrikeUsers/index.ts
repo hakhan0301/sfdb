02c992b7af3952c33fc801f75c7a7b3544e24d27
Hassan <hakhan0301@gmail.com>
2022-09-05 21:19:27 -0500

    feature: strike users function

---
+++ apps/functions/src/StrikeUsers/index.ts
@@ -0,0 +1,60 @@
+import { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import * as moment from 'moment-timezone';
+
+import { ExpoPushMessage, ExpoPushTicket } from 'expo-server-sdk/build/ExpoClient';
+import { Expo } from 'expo-server-sdk';
+
+import supabase from '@sfdb/supabase';
+
+if (!process.env.FUNCTION_SECRET)
+  throw new Error('Missing env-var: "FUNCTION_SECRET"');
+
+const expo = new Expo();
+
+process.env.TZ = 'America/Mexico_City';
+
+export const handler = async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
+  if (event?.queryStringParameters?.key !== process.env.FUNCTION_SECRET)
+    return unauthorized(event?.queryStringParameters?.key);
+
+  const { data: strikedUsers, error } = await supabase
+    .rpc('strike_users')
+
+  if (error)
+    return {
+      body: JSON.stringify(error),
+      statusCode: 500
+    }
+
+  const pushMessages = strikedUsers
+    .filter(user => user.notification_token)
+    .map((user): ExpoPushMessage => ({
+      title: 'sfdb - WARNING',
+      to: user.notification_token!,
+      body: `You did not post today, you have been striked. ${3 - user.strikes} more and you're banned.`
+    }));
+
+  const chunks = expo.chunkPushNotifications(pushMessages);
+
+  let results: ExpoPushTicket[] = [];
+  for (const chunk of chunks) {
+    const res = await expo.sendPushNotificationsAsync(chunk);
+    results.push(...res);
+  }
+
+  return {
+    body: JSON.stringify(strikedUsers),
+    statusCode: 200
+  }
+}
+
+function unauthorized(key: string | undefined): APIGatewayProxyResult {
+  return {
+    body: `Invalid "key" in querystring. Received: ${key}`,
+    statusCode: 400
+  }
+}
+
+
